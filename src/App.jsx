6f7226efd4c6a3c41e9abae6c22cbbcf1b34ed91
nisarlala0987-boop <nisarlala0987@gmail.com>
2026-08-12 15:11:23 +0500

    initalize react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the App!</h1>
+    </div>
+  );
+}
+
+export default App;
